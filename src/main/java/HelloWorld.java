ec63426502d51a53b46bfd774aa3b5e3279b22c7
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2023-06-14 16:22:16 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World at 14 Jun 23 - 9.44");
+		System.out.println("Hello World at 14 Jun 23 - 4.22");
 	}
 
 }
